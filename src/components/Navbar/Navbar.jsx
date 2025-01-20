d2ed4aa69dc825740efaad60ae0a8713e6fa98ca
urprakashgupta <urprakashgupta@gmail.com>
2025-01-20 22:32:54 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import { useState } from "react";
 import "./navbar.scss";
-export default function Navbar() {
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
-    <nav className="nav">
+    <nav>
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>UR Real Estate</span>
+          <span>LamaEstate</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
-        <a href="/">Content</a>
+        <a href="/">Contact</a>
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        <a href="/">Sign In</a>
-        <a href="/" className="register">
-          Sign Up
-        </a>
+        <>
+          <a href="/">Sign in</a>
+          <a href="/" className="register">
+            Sign up
+          </a>
+        </>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
 }
+
+export default Navbar;
